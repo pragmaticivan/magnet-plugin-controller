f59d5d479fe1c4695bba048621b0088c5a1c061d
Ivan Santos <pragmaticivan@gmail.com>
2017-05-01 12:11:06 -0700

    🚀 Adds base 🐘

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import {isFunction, isObject} from 'metal';
+
+const isMultiple = module => {
+  return isObject(module.route) && module.route.multiple;
+};
+
+export default {
+  test(module, filename, magnet) {
+    return isMultiple(module) && isFunction(module.default);
+  },
+
+  register(module, filename, magnet) {
+    const app = magnet.getServer().getEngine();
+    module.default.call(module.default, app, magnet);
+  },
+};
