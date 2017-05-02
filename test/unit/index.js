5fbbab8d6ee21a240a4bde01825890105eb2e22b
Ivan Santos <pragmaticivan@gmail.com>
2017-05-01 18:13:53 -0700

    Adds tests

---
+++ test/unit/index.js
@@ -0,0 +1,46 @@
+import Magnet from 'magnet';
+import pluginMultiple from '../../src/index';
+
+describe('pluginMultiple', () => {
+  describe('.test', () => {
+    it('should return true if module has route multiple', () => {
+      const mod = {};
+      mod.route = {multiple: true};
+      mod.default = (app, magnet) => {};
+      expect(pluginMultiple.test(mod, null, null)).to.be.true;
+    });
+
+    it('should return false if module is not function', () => {
+      const mod = {};
+      mod.route = {multiple: true};
+      mod.default = 'not a function';
+      expect(pluginMultiple.test(mod, null, null)).to.be.false;
+    });
+  });
+
+  describe('integration', () => {
+    let magnet;
+    const directory = `${process.cwd()}/test/fixtures/multiple`;
+
+    beforeEach(async () => {
+      magnet = new Magnet({directory});
+      await magnet.build();
+      await magnet.start();
+    });
+
+    afterEach(async () => {
+      await magnet.stop();
+    });
+
+    it('should register multiple module from directory', async () => {
+      await assertAsyncHttpRequest({
+        path: '/route-one',
+        responseBody: 'one',
+      });
+      await assertAsyncHttpRequest({
+        path: '/route-two',
+        responseBody: 'two',
+      });
+    });
+  });
+});
